4e7f156ddb4d0d816d7e9804b709112ff1d6776a
LUAN JUSTINIANO DOS SANTOS <luanj12183@gmail.com>
2026-05-22 23:33:24 +0000

    ...

--- minhpaginaweb/Controllers/HomeController.cs
+++ minhpaginaweb/Controllers/HomeController.cs
@@ -8,17 +8,13 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
-    public IActionResult Index()
-{
+
     ViewBag.Nome = "Luan"; 
     ViewBag.Curso = "Análise e Desenvolvimento de Sistemas";
     ViewBag.Semestre = "3º Semestre";
-    
     return View();
-}
-
-        return View();
     }
+
 
     public IActionResult Privacy()
     {
