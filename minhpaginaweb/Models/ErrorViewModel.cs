b71cb2a4ea4dcb1f5fc45a107b878e5898c75aed
LUAN JUSTINIANO DOS SANTOS <luanj12183@gmail.com>
2026-05-22 21:23:50 +0000

    Primeiro commit da minha app.NET

---
+++ minhpaginaweb/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace minhpaginaweb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
