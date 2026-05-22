272616e7e7d5578bd41761a3c844445c9176f05a
LUAN JUSTINIANO DOS SANTOS <luanj12183@gmail.com>
2026-05-22 23:07:16 +0000

    Novas Informações e pastas

---
+++ minhpaginaweb/Controllers/Controllers/AlunoController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace minhapaginaweb.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Nome = "Luan"; 
+            ViewBag.Curso = "Análise e Desenvolvimento de Sistemas";
+            ViewBag.Semestre = "3º Semestre";
+            return View();
+        }
+
+        public IActionResult Detalhes(int id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+    }
+}
